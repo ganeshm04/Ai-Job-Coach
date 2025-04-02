fa15956a10e0d93cad31016059e01a953b5e3f62
Ganesh M <ganeshmeti2004@gmail.com>
2025-04-02 09:48:39 +0530

    Hero Section Added

---
+++ app/page.jsx
@@ -0,0 +1,22 @@
+import HeroSection from "@/components/hero";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <div className="grid-background"></div>
+
+      <HeroSection />
+
+      <section>
+        <div>
+          <h2>
+            Powerful Features 
+          </h2>
+        </div>
+      </section>
+
+    </>
+  );
+}
