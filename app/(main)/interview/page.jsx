b58b7bd9d5bec66101a7ee316ec0e23c36233b1e
Ganesh M <ganeshmeti2004@gmail.com>
2025-04-10 16:11:26 +0530

    interview preparation page created , mock quiz page is created

--- app/(main)/interview/page.jsx
+++ app/(main)/interview/page.jsx
@@ -1,9 +1,28 @@
+import { getAssessments } from '@/actions/interview'
 import React from 'react'
+import StatsCards from './_components/stats-card';
+import PerformanceChart from './_components/performance-chart';
+import QuizList from './_components/quiz-list';
 
-const Page = () => {
+const InterviewPage = async () => {
+
+  const assessments = await getAssessments();
+
+
+
   return (
-    <div>Interview Page</div>
+
+    <div>
+      <h1 className='text-6xl font-bold gradient-title mb-5'>Interview Preparation</h1>
+
+    <div className='space-y-6'>
+      <StatsCards assessments={assessments}/>
+      <PerformanceChart assessments={assessments}/>
+      <QuizList assessments={assessments}/>
+    </div>
+    </div>
+
   )
 }
 
-export default Page
+export default InterviewPage
