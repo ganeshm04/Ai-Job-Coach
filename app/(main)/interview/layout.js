0c78dc67f28ad1970c8c0ce0e6900475c818fe15
Ganesh M <ganeshmeti2004@gmail.com>
2025-05-03 13:41:17 +0530

    Final Changes

--- app/(main)/interview/layout.js
+++ app/(main)/interview/layout.js
@@ -5,7 +5,6 @@ const Layout = ({children}) => {
   return (
     <div className='px-5'>
         <div className='flex items-center justify-between mb-5'>
-           <h1 className='text-6xl font-bold gradient-white'>Industry Insights </h1>
         </div>
         <Suspense fallback={<BarLoader className='mt-4' width={"100%"} color='gray'/>}>
         {children}
